0ac963c05d5a62b5d024fe05bffcdb8f09415d6c
Angelina <ani-bri@ya.ru>
2023-02-06 18:01:27 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './styles.css'
+
+import './styles.css'
+
+const coonter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+
+let state = 0
+
+function render() {
+    counter.textContent = state.toString()
+}
+
+addBtn.addEventListener('click', () => {
+    state++
+    render()
+})
+
+addBtn.addEventListener('click', () => {
+    state--
+    render()
+})
+
+render()
